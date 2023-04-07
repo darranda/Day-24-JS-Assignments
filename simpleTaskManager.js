4f9210deed1d2ce786381f18412e3ab5de09a96b
unknown <darrandaf@gmail.com>
2023-04-06 22:44:18 -0400

    js assignments edit

--- simpleTaskManager.js
+++ simpleTaskManager.js
@@ -1,18 +1,18 @@
-// Create an IIFE named taskManager that contains the following functionality:
-// A private variable named tasks to store task descriptions. It should be initialized as an empty array.
+/* Create an IIFE named taskManager that contains the following functionality: A private variable named tasks to store task descriptions. It should be initialized as an empty array.*/
 
 let taskManager = (function (){
     let tasks = [];
 
 // A addTask function that takes a task description as an argument and adds it to the tasks array.
+
 function addTask (taskDescription) {
-    tasks.push(taskDescription)
+    tasks.push(taskDescription);
     
 }
 
 // A displayTasks function that logs all tasks in the tasks array to the console.
 function displayTasks () {
-    console.log (tasks)
+    console.log (tasks);
 }
 
 // The IIFE should return an object with the addTask and displayTasks functions.
@@ -27,4 +27,4 @@ function displayTasks () {
     taskManager.addTask ('wash dishes');
     taskManager.addTask ('complete CTAC assignments');
 
-    taskManager.displayTasks ();
+    taskManager.displayTasks ('');
