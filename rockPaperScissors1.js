7c794ec494ecbc43a014c61ea5b321a4ff810625
unknown <darrandaf@gmail.com>
2023-04-07 17:55:49 -0400

    last commit

--- rockPaperScissors1.js
+++ rockPaperScissors1.js
@@ -5,7 +5,7 @@ let hands = ['rock', 'paper', 'scissors'];
 //Define a function called getHand() that returns a hand from the array using parseInt(Math.random()*10)%3
 
 function getHand() {
-return hand (parseInt(Math.random()*10)%3)
+return (parseInt(Math.random()*10)%3)
 }
 
 //Define two objects for two players. Each player has name and getHand() properties.
@@ -16,41 +16,45 @@ let onePlayer = {
 }
 let twoPlayer = {
     name: 'Reese',
-    getHand 
+    getHand
 }
-console.log(onePlayer.name)
+console.log(onePlayer)
 
-//Define a function called playRound() that //Takes two player objects as arguments
-function playRound (a,b)
+/*Define a function called playRound() that Takes two player objects as arguments
+ Gets hands from each
+ Determines the winner
+Logs the hands played and name of the winner.
+If its a tie, log the hands played and "it's a tie".
+Returns the winner object (null if no winner) */
 
-//Gets hands from each
-onePlayer.getHand();
-twoPlayer.getHand();
+function playRound (a,b) {
+    let sarahHand = onePlayer.getHand();
+    let reeseHand = twoPlayer.getHand();
 
-//Determines the winner
-//Logs the hands played and name of the winner.
-//If its a tie, log the hands played and "it's a tie".
-//Returns the winner object (null if no winner)
-let winner = null
-if (onePlayerHand = twoPlayerHand)  {
-    console.log (`playersHand: ${Sarah} played ${onePlayerHand} and ${Reese} played ${twoPlayerHand}. Its a tie!`);
+    if (sarahHand == reeseHand)  {
+        //console.log (`playersHand: ${onePlayer.name} hand played ${sarahHand} while ${twoPlayer.name} hand played ${reeseHand}. Its a tie!`);
+    }
+    else if
+        ((sarahHand == 'scissors'&& reeseHand == 'paper') ||
+        (sarahHand == 'rock' && reeseHand == 'scissors') || 
+        (sarahHand == 'paper' && reeseHand == 'rock'))
+    {
+        winner = onePlayer;
+        //console.log (`playerHand: ${onePlayer.name} hand played ${sarahHand}. ${twoPlayer.name} hand played ${reeseHand}. Sarah is the winner!`);
+    } 
+    else
+    
+        ((reeseHand == 'rock' && sarahHand == 'scissors') ||
+        (reeseHand == 'scissors' && sarahHand == 'paper') ||
+        (reeseHand == 'paper' && sarahHand == 'rock')); 
+   { 
+        winner = twoPlayer;
+        //console.log (`playerHand: ${twoPlayer.name} hand played ${reeseHand}. ${onePlayer.name} hand played ${sarahHand}. Reese is the winner!`) 
+    }
 }
-else if ((onePlayerHand == 'scissors'&& twoPlayerHand == 'paper') ||
-    (onePlayerHand == 'rock' && twoPlayerHand == 'scissors') || 
-    (onePlayerHand == 'paper' && twoPlayerHand == 'rock'))
-{
-    winner = onePlayer;
-    console.log ()
-} 
-else {
-    ((twoPlayerHand == 'rock' && onePlayerHand == 'scissors') ||
-    (twoPlayerHand == 'scissors' && onePlayerHand == 'paper') ||
-    (twoPlayerHand == 'paper' && onePlayerHand == 'rock')) 
-}
-{
-    winner = twoPlayer;
-    console.log ()
 
-}
-  playRound (onePlayer, twoPlayer)
-  
+    let winner = null
+    
+    playRound (onePlayer, twoPlayer)
+
+    console.log(winner)
